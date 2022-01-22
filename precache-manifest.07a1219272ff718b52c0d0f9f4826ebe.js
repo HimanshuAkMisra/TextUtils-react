07325e87c569c6e501890ba84ce838a937714383
HimanshuAkMisra <mshrah0222@gmail.com>
2022-01-22 17:36:35 +0530

    Add files via upload

---
+++ precache-manifest.07a1219272ff718b52c0d0f9f4826ebe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4611e4d2e72903543186c2d7d40dc01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e3a7b124b13cbc965cb",
+    "url": "/static/css/main.16e18a48.chunk.css"
+  },
+  {
+    "revision": "b5a9c563fc747d68dfd4",
+    "url": "/static/js/2.1969e564.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.1969e564.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e81051879e360fe41af8",
+    "url": "/static/js/3.7d17f339.chunk.js"
+  },
+  {
+    "revision": "1e3a7b124b13cbc965cb",
+    "url": "/static/js/main.082fdfe8.chunk.js"
+  },
+  {
+    "revision": "51311f3e7ba0b4394a17",
+    "url": "/static/js/runtime-main.352774dc.js"
+  }
+]);
